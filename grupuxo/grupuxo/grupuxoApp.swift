bdd0ecd312d92a7cbe4ba9a1721540ad4cb9b908
Henriquelacerdas <hen.lac.sil@gmail.com>
2026-09-14 08:35:49 -0300

    inicializa projeto xcode

---
+++ grupuxo/grupuxo/grupuxoApp.swift
@@ -0,0 +1,17 @@
+//
+//  grupuxoApp.swift
+//  grupuxo
+//
+//  Created by Henrique Lacerda Silveira on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct grupuxoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ grupuxo/grupuxoTests/grupuxoTests.swift
@@ -0,0 +1,19 @@
+//
+//  grupuxoTests.swift
+//  grupuxoTests
+//
+//  Created by Henrique Lacerda Silveira on 14/09/26.
+//
+
+import Testing
+@testable import grupuxo
+
+struct grupuxoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
